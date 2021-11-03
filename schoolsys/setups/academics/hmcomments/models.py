a0ffb09cdbbd57eef53f78e8faabcef6e412badc
BIXENTEKIM <james@rensoft.co.ke>
2021-11-03 05:52:23 +0300

    Adding hmcomments

---
+++ schoolsys/setups/academics/hmcomments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from setups.academics.departments.models import SchoolDepartments
+
+
+class GradesGrid(models.Model):
+    grades_code = models.AutoField(primary_key=True)
+    grades_val1 = models.IntegerField(default=0)
+    grades_val2 = models.IntegerField(default=0)
+    grades_grade = models.CharField(max_length=20 )
+    grades_remarks = models.CharField(max_length=50 )
+    grades_department = models.ForeignKey(SchoolDepartments, on_delete=models.CASCADE, null=False, blank=False,default=0)
+    grades_option = models.CharField(max_length=50,null=True,blank=True,default="ALL" )
